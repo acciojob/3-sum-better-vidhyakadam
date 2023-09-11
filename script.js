0d61b8a51ae2c38301511b31b074ba87f86100a7
Vidhya Parmeshwar Kadam <131935689+vidhyakadam@users.noreply.github.com>
2023-09-11 19:04:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 function threeSum(arr, target) {
-// write your code here
-	//Sort the input array 
-	arr.sort((a, b) => a-b);
-	let closestSum = Infinity;
-	for(let i=0; i<arr.length-2; i++){
-		let left = i+1;
-		let right = arr.length-1;
-		while(left>right){
-			const currentSum = arr[i]+arr[left]+arr[right];
+  // Sort the input array
+  arr.sort((a, b) => a - b);
 
-			if(Math.abs(currentSum - target) < Math.abs(closestSum-target)){
-				closestSum = currentSum;
-			}
+  let closestSum = Infinity;
 
-			if(currentSum < target){
-				left++;
-			}
-			else {
-				right--;
-			}
-		}
-	}
-	
-	
-  return closestSum
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      const currentSum = arr[i] + arr[left] + arr[right];
+
+      if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+        closestSum = currentSum;
+      }
+
+      if (currentSum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return closestSum;
 }
 
-module.exports = threeSum;
+// Example usage:
+const arr = [-1, 2, 1, -4];
+const target = 1;
+console.log(threeSum(arr, target)); // Output: 2
